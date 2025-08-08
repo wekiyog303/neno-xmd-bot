d41c5a32f4d9f4941fb14c47ba2dd83761e7b637
Nimeshkamihiran <nimeshmihiranga183@gmail.com>
2025-08-08 09:08:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
+
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
+}
+
+module.exports = {
+  SESSION_ID: process.env.SESSION_ID || "p7dF0JII#5bshW_L0TA40JQ0be15G9H-ak-nMjFF0MRsdkQLIm2E",
+  OWNER_NUM: process.env.OWNER_NUM || "94721584279",
+  PREFIX: process.env.PREFIX || ".",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
+  MODE : process.env.MODE || "public", 
+  AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "true", 
+  AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING || "true"), 
+  ANTI_DELETE: convertToBool(process.env.ANTI_DELETE || "true"),
+};
