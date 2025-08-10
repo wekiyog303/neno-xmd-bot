08b90c10e42f95293fcdf898ca86f57167d38505
NIMESHKA MIHIRAN ❤️💪 <nimeshmihiranga183@gmail.com>
2025-08-10 12:53:53 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -22,18 +22,18 @@ cmd(
           image: {
             url: "https://files.catbox.moe/en1jzg.jpg",
           },
-          caption: `I♥A♥M♥ 𝐀𝐋𝐈𝐕𝐄 𝐍𝐎𝐖 🫡  
+          caption: `❤️𝙽𝙴𝙽𝙾 𝚇𝙼𝙳 𝚈𝙾𝚄𝚁 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃 𝙰𝙻𝙸𝚅𝙴 𝙽𝙾𝚆🤍
   
 𝗼𝗳𝗳𝗶𝗰𝗶𝗮𝗹 𝘄𝗵𝗮𝘁𝘀𝗮𝗽𝗽 𝗰𝗵𝗮𝗻𝗲𝗹 -: https://whatsapp.com/channel/0029Vb6BQQmFnSz7bmxefu40
 
-𝗚𝗶𝘁 𝗛𝘂𝗯 𝗥𝗲𝗽𝗼 -: 𝐂𝐎𝐌𝐈𝐍𝐆 𝐒𝐎𝐎𝐍 !!😉
+𝗚𝗶𝘁 𝗛𝘂𝗯 𝗥𝗲𝗽𝗼 -: https://github.com/Nimeshkamihiran/neno-xmd-bot
 
 𝗢𝘄𝗻𝗲𝗿 -: https://wa.me/94721584279?text=𝐇𝐈_𝐍𝐄𝐎𝐍_𝐗𝐌𝐃_𝐍𝐄𝐖_𝐔𝐒𝐄𝐑_𝐈_𝐍𝐄𝐄𝐃_𝐇𝐄𝐋𝐏🫡
           
-*We are not responsible for any*  
-*WhatsApp bans that may occur due to*  
-*the usage of this bot. Use it wisely*  
-*and at your own risk* ⚠️`,
+*𝙸 𝙻𝙾𝚅𝙴 𝙰𝙻𝙻 𝚄𝚂𝙴𝚁𝚂 ❤️💪*  
+*SHERE THIS ALIVE MSG AND SUP OURE DEVLOPERS*  
+*PLZ JOIN OUR WHATSAPP GROUP*  
+*🄽🄴🄽🄾 🅇🄼🄳 🅄🅂🄴🅁 🄵🅁🄴🄽🄳🄻🅈 `,
         },
         { quoted: mek }
       );
