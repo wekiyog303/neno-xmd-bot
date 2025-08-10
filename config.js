fae597475987670706638efcf3cb512b54a42320
NIMESHKA MIHIRAN ❤️💪 <nimeshmihiranga183@gmail.com>
2025-08-10 12:39:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "ZrUQgLqC#bzhqqihZmTUleCVunbM83F-UYT1mhZOP4uHhkCRDmkA",
+  SESSION_ID: process.env.SESSION_ID || "5rl11RpI#lpRuJz5RwAx0TivsMpoMzcTSt9hxLMqCJrTCbtmL0wM",
   OWNER_NUM: process.env.OWNER_NUM || "94721584279",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
