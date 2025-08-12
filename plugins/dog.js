ffd91bf1b8dce69f7103037adf2ccca2cd260a26
NIMESHKA MIHIRAN ❤️💪 <nimeshmihiranga183@gmail.com>
2025-08-13 04:46:09 +0530

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: 'NENO XMD DOWNLOAD📂DOG PICS\n\n\n> *BY MS SANA*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: 'NENO XMD DOWNLOAD📂DOG PICS\n\n\n> *BY NIMESHKA*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error Fetching Dog Image🤕: ${e.message}`);
