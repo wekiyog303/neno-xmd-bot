9797facdd1af1ff6fbc9b4fa7a7bddecdddc21c1
NIMESHKA MIHIRAN ❤️💪 <nimeshmihiranga183@gmail.com>
2025-08-12 11:16:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,13 +10,16 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "5rl11RpI#lpRuJz5RwAx0TivsMpoMzcTSt9hxLMqCJrTCbtmL0wM",
   OWNER_NUM: process.env.OWNER_NUM || "94721584279",
   PREFIX: process.env.PREFIX || ".",
-  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-  AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
-  MENU_IMG: process.env.MENU_IMG || "https://files.catbox.moe/43e40k.jpg",
+  
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",      // Status seen karanna ona da
+  AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",    // Status walata react karanna ona da
+  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",    // Status walata reply karanna ona da
+  AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "Thanks for sharing your status!",  // Status reply message
+  
   MODE : process.env.MODE || "public", 
+  
   AUTO_STATUS_LIKE: process.env.AUTO_STATUS_LIKE || "true", 
   AUTO_RECORDING: convertToBool(process.env.AUTO_RECORDING || "true"), 
-  AUTO_CHAT: true,
   ANTI_DELETE: convertToBool(process.env.ANTI_DELETE || "true"),
   ANTI_BAD: process.env.ANTI_BAD || "true",
 };
