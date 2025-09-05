fb6b547396402ab28745c2d7414c3ac600ef03e1
wekiyog303 <yisoy9271@gmail.com>
2025-09-05 22:41:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "LLIyyKZC#S43pzCFDuyarnK0iT30js0exszEMQHzBymXk_jIem7M",
-  OWNER_NUM: process.env.OWNER_NUM || "94726228243",
+  SESSION_ID: process.env.SESSION_ID || "NENO-XMD~iKpziDQR#1ZL-2ILP_nBsgdx9U7HzRtSTgQzEUO0hzB0JXVYYUDY",
+  OWNER_NUM: process.env.OWNER_NUM || "212647928682",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
